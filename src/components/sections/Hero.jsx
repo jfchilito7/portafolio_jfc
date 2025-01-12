b151ca25c2c252152cc0d230075d60841e6b9ed2
jfchilito7 <jfchilito7@gmail.com>
2025-01-11 23:47:04 -0500

    Agregar efecto de máquina de escribir en el componente Hero y estilizar nuevos elementos

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { BioData } from '../../data/constants';
+import Typewriter from 'typewriter-effect';
 
 const HeroContainer = styled.div`
     display: flex;
@@ -39,6 +40,7 @@ const HeroLeftContainer = styled.div`
         margin-bottom: 30px;
         display: flex;
         flex-direction: column;
+        gap: 6px;
         align-items: center;
 
     @media screen and (max-width: 640px) {
@@ -87,6 +89,68 @@ const Title = styled.div `
     }
 `;
 
+const TextLoop = styled.div `
+    font-weight: 600;
+    font-size: 32px;
+    display: flex;
+    color: ${({theme}) => theme.text_primary};
+    line-height: 68px;
+    gap: 12px;
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+    }
+
+    @media screen and (max-width: 960px) {
+        font-size: 22px;
+        line-height: 48px;
+        margin-bottom: 16px;
+    }
+`;
+
+const Span = styled.div `
+    cursor: pointer;
+    color: ${({theme}) => theme.primary};
+`;
+
+const SubTitle = styled.div`
+    font-size: 20px;
+    line-height: 32px;
+    margin-buttom: 42px;
+    color: ${({theme}) => theme.text_primary + 95};
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+    }
+
+    @media screen and (max-width: 960px) {
+        font-size: 16px;
+        line-height: 32px;
+    }
+`;
+
+const ResumeButton = styled.a `
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+    text-decoration: none;
+
+    width: 95%;
+    max-width: 300px;
+    text-align: center;
+    padding: 16px 0;
+    margin-top: 42px;
+    
+    background: hlsa(271, 100%, 50%, 1);
+    background: linear-gradient(225deg, hsla(204, 100%, 50%, 1) 0%, hsla(204, 100%, 50%, 1) 100%);
+    background: -moz-linear-gradient(225deg, hsla(204, 100%, 50%, 1) 0%, hsla(204, 100%, 50%, 1) 100%);
+    background: -webkit-linear-gradient(225deg, hsla(204, 100%, 50%, 1) 0%, hsla(204, 100%, 50%, 1) 100%);
+    box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+    border-radius: 50px;
+    font-weight: 700;
+    font-size: 18px;
+`
+
 const Hero = () => {
     return (
         <div id='about'>
@@ -96,6 +160,20 @@ const Hero = () => {
                         <Title>
                             Hola! Soy <br /> {BioData.name}
                         </Title>
+                        <TextLoop>
+                            Yo soy
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: BioData.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                /> 
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{BioData.description}</SubTitle>
+                        <ResumeButton>Descargar CV</ResumeButton>
                     </HeroLeftContainer>
                     <HeroRightContainer>Right</HeroRightContainer>
                 </HeroInnerContainer>
