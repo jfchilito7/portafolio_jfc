f52e89b4f0cced69c8776f1e550a286415568841
jfchilito7 <jfchilito7@gmail.com>
2025-01-15 10:43:03 -0500

    Agregar componentes Skills y Experience; actualizar App para incluir nuevas secciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { darkTheme } from './utils/Themes';
 import Navbar from './components/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import Hero from './components/sections/Hero';
+import Skills from './components/sections/Skills';
+import Experience from './components/sections/Experience';
 
 const Body = styled.div`
   background-color: ${({theme}) => theme.bg};
@@ -23,6 +25,7 @@ function App() {
           <Body>
             <Hero />
             <Skills />
+            <Experience />
           </Body>
         </BrowserRouter>
       </ThemeProvider>
