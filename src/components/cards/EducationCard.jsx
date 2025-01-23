03e126329732ed96bd2527c1425908c3c9fd4943
jfchilito7 <jfchilito7@gmail.com>
2025-01-23 11:10:18 -0500

    Agregar validación de PropTypes en EducationCard y mejorar la legibilidad del código

--- src/components/cards/EducationCard.jsx
+++ src/components/cards/EducationCard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
+import PropTypes from 'prop-types';
 
 const Top = styled.div`
     width: 100%;
     display: flex;
-    max-width: 100%;
     gap: 12px;
 `;
 
@@ -28,17 +28,17 @@ const Body = styled.div`
 const School = styled.div`
     font-size: 18px;
     font-weight: 600;
-    color: ${({theme}) => theme.text_primary + 99};
+    color: ${({ theme }) => theme.text_primary + 99};
 
     @media screen and (max-width: 768px) {
         font-size: 14px;
-    }  
+    }
 `;
 
 const Degree = styled.div`
     font-size: 14px;
     font-weight: 500;
-    color: ${({theme}) => theme.text_secondary + 99};
+    color: ${({ theme }) => theme.text_secondary + 99};
 
     @media screen and (max-width: 768px) {
         font-size: 12px;
@@ -48,7 +48,7 @@ const Degree = styled.div`
 const Date = styled.div`
     font-size: 12px;
     font-weight: 400;
-    color: ${({theme}) => theme.text_secondary + 80};
+    color: ${({ theme }) => theme.text_secondary + 80};
 
     @media screen and (max-width: 768px) {
         font-size: 10px;
@@ -59,7 +59,7 @@ const Description = styled.div`
     width: 100%;
     font-size: 15px;
     font-weight: 400;
-    color: ${({theme}) => theme.text_primary + 99};
+    color: ${({ theme }) => theme.text_primary + 99};
     margin-bottom: 10px;
 
     @media screen and (max-width: 768px) {
@@ -75,61 +75,73 @@ const Span = styled.div`
 const Grade = styled.div`
     font-size: 14px;
     font-weight: 500;
-    color: ${({theme}) => theme.text_secondary + 99};
+    color: ${({ theme }) => theme.text_secondary + 99};
 
     @media screen and (max-width: 768px) {
         font-size: 12px;
     }
 `;
 
-
-
 const EducationCard = ({ education }) => {
     return (
         <VerticalTimelineElement
             icon={
-            <img 
-                width = '100%'
-                height = '100%'
-                alt={education?.school}
-                style={{borderRadius: '50%', objectFit: 'cover'}}
-                src={education?.img}
-            />
-        }
-        contentStyle={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '12px',
-            background: '#1d1836',
-            color: '#fff',
-            boxShadow: 'rgba(23, 92, 230, 0.15) 0px 4px 24px',
-            backgroundColor: 'rgba(17, 25, 40, 0.83)',
-            border: '1px solid rgba(255, 255, 255, 0.125)',
-            borderRadius: '6px',
-        }}
-
-        contentArrowStyle={{
-            borderRight: '7px solid  rgba(255, 255, 255, 0.3)',
-        }}
-        date={education?.date}
+                <img
+                    width="100%"
+                    height="100%"
+                    alt={education?.school || 'School logo'}
+                    style={{ borderRadius: '50%', objectFit: 'cover' }}
+                    src={education?.img}
+                />
+            }
+            contentStyle={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '12px',
+                background: '#1d1836',
+                color: '#fff',
+                boxShadow: 'rgba(23, 92, 230, 0.15) 0px 4px 24px',
+                backgroundColor: 'rgba(17, 25, 40, 0.83)',
+                border: '1px solid rgba(255, 255, 255, 0.125)',
+                borderRadius: '6px',
+            }}
+            contentArrowStyle={{
+                borderRight: '7px solid  rgba(255, 255, 255, 0.3)',
+            }}
+            date={education?.date}
         >
             <Top>
-                <Image src={education?.img} alt={education.school} />
+                <Image src={education?.img} alt={education?.school || 'School logo'} />
                 <Body>
                     <School>{education?.school}</School>
                     <Degree>{education?.degree}</Degree>
                     <Date>{education?.date}</Date>
                 </Body>
             </Top>
-            <Grade>
-                <b>Grade : </b>
-                {education?.grade}
-            </Grade>
+            {education?.grade && (
+                <Grade>
+                    <b>Grade: </b>
+                    {education.grade}
+                </Grade>
+            )}
+            {education?.desc && (
                 <Description>
-                    {education?.desc && <Span>{education.desc}</Span>} 
+                    <Span>{education.desc}</Span>
                 </Description>
+            )}
         </VerticalTimelineElement>
     );
+};
+
+EducationCard.propTypes = {
+    education: PropTypes.shape({
+        school: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        degree: PropTypes.string,
+        date: PropTypes.string,
+        grade: PropTypes.string,
+        desc: PropTypes.string,
+    }).isRequired,
 };
 
 export default EducationCard;
